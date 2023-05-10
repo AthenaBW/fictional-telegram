1059a0825d32a6f2b14eca2380e0b35e7803a190
Athena <athenawilbon1994@aol.com>
2023-05-09 21:06:42 -0400

    Comments fixed and added more

--- assets/script.js
+++ assets/script.js
@@ -15,103 +15,114 @@ function displayWeather(event) {
 
     city = citySearch.val();
     currentWeather(city);
-  }
+}
 
-  // AJAX call
-  function currentWeather(city) {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +  city + "&APPID=" + APIKey;
+// AJAX call
+function currentWeather(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
 
-    $.ajax({ url: queryURL, method: "GET",
+    $.ajax({
+        url: queryURL, method: "GET",
     }).then(function (response) {
-        
-      var weatherIcon = response.weather[0].icon;
 
-      var iconUrl = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
-  
-      var date = new Date(response.dt * 1000).toLocaleDateString();
+        var weatherIcon = response.weather[0].icon;
 
-      $(cityEl).html( response.name + "(" + date + ")" + "<img src=" + iconUrl + ">");
-  
-      // Convert the temp to fahrenheit
-  
-      var Ftemp = (response.main.temp - 273.15) * 1.8 + 32;
+        var iconUrl = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
 
-      $(currentTempEl).html(Ftemp.toFixed(0) + " F");
-  
-      $(currentHumidityEl).html(response.main.humidity + "%");
-  
-      // convert wind to MPH
-      var ws = response.wind.speed;
-      var windsmph = (ws * 2.237).toFixed(0);
-      $(currentWindEl).html(windsmph + " MPH");
-  
-      // set local storage
-      forecast(response.id);
-      if (response.id !== "null") {
-        cityS = JSON.parse(localStorage.getItem("cityname"));
-  
-        if (cityS == null) {
-          cityS = [];
-          cityS.push(city);
-          localStorage.setItem("cityname", JSON.stringify(cityS));
-          addToList(city);
-        } else {
-          if (find(city) > 0) {
-            cityS.push(city);
-            localStorage.setItem("cityname", JSON.stringify(cityS));
-            addToList(city);
-          }
+        var date = new Date(response.dt * 1000).toLocaleDateString();
+
+        $(cityEl).html(response.name + "(" + date + ")" + "<img src=" + iconUrl + ">");
+
+        // Convert the temp to fahrenheit
+
+        var Ftemp = (response.main.temp - 273.15) * 1.8 + 32;
+
+        $(currentTempEl).html(Ftemp.toFixed(0) + " F");
+
+        $(currentHumidityEl).html(response.main.humidity + "%");
+
+        // convert wind to MPH
+        var ws = response.wind.speed;
+        var windsmph = (ws * 2.237).toFixed(0);
+        $(currentWindEl).html(windsmph + " MPH");
+
+        // Checks if the city id retrieved from the API response is not null
+        // If not , retrieves array of city from local storage
+        forecast(response.id);
+        if (response.id !== "null") {
+            cityS = JSON.parse(localStorage.getItem("cityname"));
+
+            if (cityS == null) {
+                cityS = [];
+                cityS.push(city);
+                localStorage.setItem("cityname", JSON.stringify(cityS));
+                addToList(city);
+                // Checks if the current city name is not already in the city names array
+            } else {
+                if (find(city) > 0) {
+                    cityS.push(city);
+                    localStorage.setItem("cityname", JSON.stringify(cityS));
+                    addToList(city);
+                }
+            }
         }
-      }
     });
-  }
-  
-  function forecast(cityid) {
+}
+
+// Function to fetch weather forecast data for a given city ID
+
+function forecast(cityid) {
     var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityid}&appid=${APIKey}`;
     $.ajax({
-      url: forecastURL,
-      method: "GET",
+        url: forecastURL,
+        method: "GET",
     }).then((response) => {
-      for (let i = 0; i < 5; i++) {
-        var date = new Date(response.list[(i + 1) * 7].dt * 1000).toLocaleDateString();
-        var iconcode = response.list[(i + 1) * 7].weather[0].icon;
-        var iconUrl = `https://openweathermap.org/img/w/${iconcode}.png`;
-        var tempA = response.list[(i + 1) * 7].main.temp;
-        var Ftemp = ((tempA - 273.5) * 1.8 + 32).toFixed(0);
-        var humidity = response.list[(i + 1) * 7].main.humidity;
-  
-        $(`#wDate${i}`).html(date);
-        $(`#wImg${i}`).html(`<img src="${iconUrl}">`);
-        $(`#wTemp${i}`).html(`${Ftemp} F`);
-        $(`#wHumidity${i}`).html(`${humidity}%`);
-      }
+        for (let i = 0; i < 5; i++) {
+            var date = new Date(response.list[(i + 1) * 7].dt * 1000).toLocaleDateString();
+            var iconcode = response.list[(i + 1) * 7].weather[0].icon;
+            var iconUrl = `https://openweathermap.org/img/w/${iconcode}.png`;
+            var tempA = response.list[(i + 1) * 7].main.temp;
+            var Ftemp = ((tempA - 273.5) * 1.8 + 32).toFixed(0);
+            var humidity = response.list[(i + 1) * 7].main.humidity;
+
+            // Update the HTML content of the forecast display elements with the extracted data
+
+            $(`#wDate${i}`).html(date);
+            $(`#wDate${i}`).html(date);
+            $(`#wImg${i}`).html(`<img src="${iconUrl}">`);
+            $(`#wTemp${i}`).html(`${Ftemp} F`);
+            $(`#wHumidity${i}`).html(`${humidity}%`);
+        }
     });
-  }
-  
-  function addToList(place) {
+}
+
+// Function to add a new place to the list of saved places
+
+function addToList(place) {
     const listEl = $(`<li>${place}</li>`)
-      .attr("class", "list-group-item")
-      .attr("data-value", place);
+        .attr("class", "list-group-item")
+        .attr("data-value", place);
     $(".list-group").append(listEl);
-  }
-  
-// local storage
+}
+
+
 function previousCity() {
+     // Remove all items from the list
     $("ul").empty();
+    // Get the list of previously searched cities from localStorage
     var cityS = JSON.parse(localStorage.getItem("cityname"));
     if (cityS !== null) {
-      cityS = JSON.parse(localStorage.getItem("cityname"));
-      for (i = 0; i < cityS.length; i++) {
-        addToList(cityS[i]);
-      }
-      city = cityS[(i -= 1)];
-  
-      currentWeather(city);
+        cityS = JSON.parse(localStorage.getItem("cityname"));
+        for (i = 0; i < cityS.length; i++) {
+            addToList(cityS[i]);
+        }
+        city = cityS[(i -= 1)];
+
+        currentWeather(city);
     }
-  }
-  
-  //Click Handlers
-  $("#search-button").on("click", displayWeather);
-  
-  $(window).on("load", previousCity);
-  
+}
+
+//Click Handlers
+$("#search-button").on("click", displayWeather);
+
+$(window).on("load", previousCity);
